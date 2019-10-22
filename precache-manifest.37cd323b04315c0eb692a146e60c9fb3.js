697d3ce605ad83ac9ad1ce7d0bcb86ee1b6b77e9
Alex <alextutovan@tutanota.com>
2019-10-22 21:33:06 +0200

    Updates

---
+++ precache-manifest.37cd323b04315c0eb692a146e60c9fb3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3eeee19ae6b3e64cbdaa929ab6053a0e",
+    "url": "/euroloop/index.html"
+  },
+  {
+    "revision": "b183b9c68b2a06c34a71",
+    "url": "/euroloop/static/css/main.e26ff718.chunk.css"
+  },
+  {
+    "revision": "a0e0056174e66700286d",
+    "url": "/euroloop/static/js/2.8b9e3a23.chunk.js"
+  },
+  {
+    "revision": "b183b9c68b2a06c34a71",
+    "url": "/euroloop/static/js/main.940336cc.chunk.js"
+  },
+  {
+    "revision": "7a5f20acb34aeeb1ab6f",
+    "url": "/euroloop/static/js/runtime~main.a3ad2b60.js"
+  },
+  {
+    "revision": "b05e4e837d23ed2ad1bc648857252d2b",
+    "url": "/euroloop/static/media/SpaceGrotesk-Regular.b05e4e83.woff"
+  },
+  {
+    "revision": "a34855319417882e2ce6fa708a79e857",
+    "url": "/euroloop/static/media/arrow.a3485531.svg"
+  },
+  {
+    "revision": "d97a319140b1d64ba40a01f579a0964f",
+    "url": "/euroloop/static/media/minus.d97a3191.svg"
+  },
+  {
+    "revision": "09e5b50dc61dcc8ca39eb249ad5fbe07",
+    "url": "/euroloop/static/media/newCalendar.09e5b50d.svg"
+  },
+  {
+    "revision": "3645b727f23bd9a94f5040028e6d0b3e",
+    "url": "/euroloop/static/media/plus.3645b727.svg"
+  }
+]);
